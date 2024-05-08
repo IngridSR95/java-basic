addf57210b5a11c55d51d0b1eeed9feb2c53e91e
IngridSR95 <sringrid95@gmail.com>
2024-05-07 22:17:44 -0300

    variáveis e métodos + concatenação de variáveis

--- meu-primeiro-projeto-java/src/MinhaClasse.java
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -2,7 +2,18 @@ public class MinhaClasse {
     
     public static void main(String[] args) {
         
-        System.out.print ("primeiro contado a sintaxe de java");
+        System.out.print ("primeiro contado a sintaxe de java\n");
+        String primeiroNome = "Ingrid";
+        String segundoNome = "Rodrigues";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.err.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
