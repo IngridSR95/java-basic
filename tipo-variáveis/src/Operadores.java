b99729e9588acc55c79b20e24c40f433ca13b9c5
IngridSR95 <sringrid95@gmail.com>
2024-05-08 15:06:40 -0300

    Operadores e operadores unários

--- tipo-variáveis/src/Operadores.java
+++ tipo-variáveis/src/Operadores.java
@@ -1,3 +1,25 @@
 public class Operadores {
-    
+    public static void main(String[] args) {
+
+        // classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        // qual o resultado das expressoes abaixo?
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        //Exibibção
+        System.out.println (nomeCompleto);
+        System.out.println (concatenacao);
+
+    }
 }
