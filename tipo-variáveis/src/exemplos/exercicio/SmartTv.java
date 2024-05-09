9f8a50ed8624cfea906981a51925a51cfdc25b74
IngridSR95 <sringrid95@gmail.com>
2024-05-09 20:16:14 -0300

    Exercício de criação e execução de um método.

---
+++ tipo-variáveis/src/exemplos/exercicio/SmartTv.java
@@ -0,0 +1,29 @@
+package exemplos.exercicio;
+
+public class SmartTv {
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 15;
+
+    public void ligar(){
+        
+        ligada = true;
+    }
+
+    public void desligar(){
+
+        ligada = false;
+    }
+}
+
+/* Vamos criar um exemplo de uma classe para representar uma SmartTV onde:
+
+    Ela tenha as características: ligada (boolean), canal (int) e volume (int);
+
+    Nossa TV poderá ligar e desligar e assim mudar o estado ligada;
+
+    Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1;
+
+    Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente. */
+
