636a4dc384df53bfbf3d8d0c43a1dc3a8b6924d7
IngridSR95 <sringrid95@gmail.com>
2024-05-25 16:29:30 -0300

    For em Arrays

---
+++ controle-fluxo/src/estruturarepeticao/ForArray.java
@@ -0,0 +1,13 @@
+package estruturarepeticao;
+
+public class ForArray {
+
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    }
+
+}
