b997678739f4e99faade94e41f6ff5bdfe586c64
IngridSR95 <sringrid95@gmail.com>
2024-05-25 15:28:28 -0300

    Condicional Encadeada

--- controle-fluxo/src/BoletimEscolar.java
+++ controle-fluxo/src/BoletimEscolar.java
@@ -1,14 +1,17 @@
 public class BoletimEscolar {
     public static void main(String[] args) throws Exception {
         
-        //Condicional Composta
-        int nota = 6;
+        //Condicional Encadeada
+        int nota = 10;
 
-        if(nota >= 7)
+        if(nota >= 7)//precisa retornar true
             System.out.println("Aprovado!");
-        
-        else
-            System.out.println("Reprovado!");
+
+            else if (nota >= 5 && nota < 7)//true or false
+                System.out.println("Prova de Recuperação!");
+
+            else
+                System.out.println("Reprovado!");
 
     }
 }
