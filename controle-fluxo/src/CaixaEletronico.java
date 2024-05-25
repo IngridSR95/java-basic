19cf28a95fe75189bd29ab9aa17eb588c5e97fe8
IngridSR95 <sringrid95@gmail.com>
2024-05-25 15:22:07 -0300

    Condicional Composta

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,15 +1,13 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
-        
+
         //Condicional Simples
         double saldo =25.0;
         double valorSolicitado = 2.0;
 
         if(valorSolicitado < saldo)
             saldo = saldo - valorSolicitado;
-        else
-            System.out.println("saldo insuficiente");
-
+        
         System.out.println(saldo);
 
     }
