33f74a85c018013d512b8505ac0835c8cbc60c73
IngridSR95 <sringrid95@gmail.com>
2024-05-25 16:18:19 -0300

    for

--- controle-fluxo/src/estruturarepeticao/ExemploFor.java
+++ controle-fluxo/src/estruturarepeticao/ExemploFor.java
@@ -2,8 +2,13 @@ package estruturarepeticao;
 
 public class ExemploFor {
 	public static void main(String[] args) {
+
+	// comando que será executado até que a 
+    // expressão de validação torne-se falsa 
 		for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
 			System.out.println(carneirinhos + " - Carneirinho(s)");
 		}
+
+		System.out.println("José dormiu");
 	}
 }
